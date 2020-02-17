81e61552fee7ed25412a2af8f2807e59857961c1
Yusuke Ogata <yusuke.ogata.0618.dev@gmail.com>
2020-02-17 15:16:39 +0900

    [add] lesson#09 サムネイルをクリックするとメイン領域に表示する

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 'use strict';
 
 {
+  /**
+   * 画像のパス
+   * @type string[]
+   */
   const images = [
     'img/pic00.png',
     'img/pic01.png',
@@ -11,17 +15,42 @@
     'img/pic06.png',
     'img/pic07.png',
   ];
+
+  /**
+   * 現在、mainに表示されている画像の番号
+   * @type number
+   */
   let currentIndex = 0;
 
+  /**
+   * main のエレメント
+   * @type HTMLElement
+   */
   const mainImage = document.getElementById('main');
+  // main 領域に現在の画像(= 0番目の画像)をセットする
   mainImage.src = images[currentIndex];
 
-  images.forEach(image => {
+  // サムネイルの生成
+  images.forEach((image, index) => {
+    // img 要素を生成する
     const img = document.createElement('img');
     img.src = image;
 
+    // li 要素を生成する
     const li = document.createElement('li');
+    
+    // li 要素に現在表示されていることを示すクラスを指定する
+    if(index === currentIndex) {
+      li.classList.add('current');
+    }
+    // li 要素をクリックすると、main 領域に画像を表示する
+    li.addEventListener('click', () => {
+      mainImage.src = image;
+    });
+
+    // li 要素にimg要素を追加する
     li.appendChild(img);
+    // ul 要素にli要素を追加する
     document.querySelector('.thumbnails').appendChild(li);
   });
 }
